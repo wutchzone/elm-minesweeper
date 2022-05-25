64c7fb2e8c56536cc7e50197e4035239822b9c31
Daniel Sedlak <daniel@sedlak.biz>
2022-05-25 21:59:45 +0200

    Prepare API of a MineSweeper model

--- src/Model/MineSweeper.elm
+++ src/Model/MineSweeper.elm
@@ -1,8 +1,14 @@
-module Model.MineSweeper exposing (getXSize, getYSize, newGame)
+module Model.MineSweeper exposing (MineSweeperModel, Tile, getXSize, getYSize, newGame)
+
+import Array
 
 
 type alias MineSweeperModel =
     { x : Int, y : Int, numberOfMines : Int }
+
+
+type alias Tile =
+    { discovered : Bool, bombsAround : Int, flagged : Bool, isBomb : Bool }
 
 
 newGame : Int -> Int -> Int -> MineSweeperModel
@@ -18,3 +24,18 @@ getXSize model =
 getYSize : MineSweeperModel -> Int
 getYSize model =
     model.y
+
+
+getTiles : MineSweeperModel -> MineSweeperModel -> Array.Array (Array.Array Tile)
+getTiles =
+    Debug.todo "implement"
+
+
+discoverTile : MineSweeperModel -> MineSweeperModel -> Int -> Int
+discoverTile =
+    Debug.todo "implement"
+
+
+flagTile : MineSweeperModel -> MineSweeperModel -> Int -> Int
+flagTile =
+    Debug.todo "implement"
