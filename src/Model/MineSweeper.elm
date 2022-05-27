d1ad55254380b0869fee1e986426d3f1ff56991e
Daniel Sedlak <daniel@sedlak.biz>
2022-05-27 21:25:40 +0200

    Implement counting bombs around and tests for it

--- src/Model/MineSweeper.elm
+++ src/Model/MineSweeper.elm
@@ -1,21 +1,86 @@
-module Model.MineSweeper exposing (MineSweeperModel, Tile, getXSize, getYSize, newGame)
+module Model.MineSweeper exposing (MineSweeperModel, Tile, discoverTile, getTiles, getXSize, getYSize, newGame)
 
-import Array
+import Array exposing (Array)
+import Debug
+import Html exposing (b)
+import List.Extra exposing (getAt, removeAt)
+import Random exposing (Seed, int, step)
 
 
 type alias MineSweeperModel =
-    { x : Int, y : Int, numberOfMines : Int }
+    { x : Int, y : Int, mines : Array Tile }
 
 
 type alias Tile =
     { discovered : Bool, bombsAround : Int, flagged : Bool, isBomb : Bool }
 
 
+makeTile : Bool -> Int -> Bool -> Bool -> Tile
+makeTile discovered bombsAround flagged isBomb =
+    Tile discovered bombsAround flagged isBomb
+
+
+isBombOnIndex : Array Tile -> Int -> Int -> Int -> Int
+isBombOnIndex mArr x y xSize =
+    let
+        index =
+            y * xSize + x
+    in
+    if x >= 0 && y >= 0 then
+        case Array.get index mArr of
+            Just tile ->
+                if tile.isBomb then
+                    1
+
+                else
+                    0
+
+            Nothing ->
+                0
+
+    else
+        0
+
+
+transformTile : Array Tile -> Int -> Int -> Tile -> Tile
+transformTile shuffledMines xsize ind tile =
+    let
+        xindex =
+            modBy xsize ind
+
+        yindex =
+            ind // xsize
+    in
+    { tile
+        | bombsAround =
+            isBombOnIndex shuffledMines (xindex + 1) yindex xsize
+                + isBombOnIndex shuffledMines (xindex - 1) yindex xsize
+                + isBombOnIndex shuffledMines xindex (yindex + 1) xsize
+                + isBombOnIndex shuffledMines xindex (yindex - 1) xsize
+                + isBombOnIndex shuffledMines (xindex + 1) (yindex + 1) xsize
+                + isBombOnIndex shuffledMines (xindex + 1) (yindex - 1) xsize
+                + isBombOnIndex shuffledMines (xindex - 1) (yindex + 1) xsize
+                + isBombOnIndex shuffledMines (xindex - 1) (yindex - 1) xsize
+    }
+
+
 {-| Instantiate instance of a new game.
 -}
-newGame : Int -> Int -> Int -> MineSweeperModel
-newGame x y numberOfMines =
-    { x = x, y = y, numberOfMines = numberOfMines }
+newGame : Int -> Int -> Int -> Seed -> MineSweeperModel
+newGame x y numberOfMines seed =
+    let
+        mines =
+            Array.toList (Array.repeat numberOfMines (makeTile False 0 False True)) ++ Array.toList (Array.repeat (x * y - numberOfMines) (makeTile False 0 False False))
+
+        shuffledMines =
+            Array.fromList (shuffleList seed mines)
+
+        calculatedMines =
+            Array.indexedMap
+                (transformTile shuffledMines x)
+                shuffledMines
+    in
+    { x = x, y = y, mines = calculatedMines }
 
 
 {-| Get size of X axis.
@@ -34,17 +99,26 @@ getYSize model =
 
 {-| Get tiles returns generated tiles of the model.
 -}
-getTiles : MineSweeperModel -> MineSweeperModel -> Array.Array (Array.Array Tile)
-getTiles =
-    Debug.todo "implement"
+getTiles : MineSweeperModel -> Array.Array Tile
+getTiles model =
+    model.mines
 
 
 {-| Discovers individual tile on given coordinates.
 Expects model and then returns modified model.
 -}
-discoverTile : MineSweeperModel -> MineSweeperModel -> Int -> Int
-discoverTile =
-    Debug.todo "implement"
+discoverTile : MineSweeperModel -> Int -> Int -> MineSweeperModel
+discoverTile prevModel x y =
+    let
+        index =
+            y * getXSize prevModel + x
+    in
+    case Array.get index prevModel.mines of
+        Just tile ->
+            { prevModel | mines = Array.set index { tile | discovered = True } prevModel.mines }
+
+        Nothing ->
+            prevModel
 
 
 {-| Flags individual tile if you think that there is a bomb on a given tile.
@@ -52,3 +126,37 @@ discoverTile =
 flagTile : MineSweeperModel -> MineSweeperModel -> Int -> Int
 flagTile =
     Debug.todo "implement"
+
+
+{-| Stolen from: <https://stackoverflow.com/questions/42207900/how-to-shuffle-a-list-in-elm>
+-}
+shuffleList : Seed -> List a -> List a
+shuffleList seed list =
+    shuffleListHelper seed list []
+
+
+shuffleListHelper : Seed -> List a -> List a -> List a
+shuffleListHelper seed source result =
+    if List.isEmpty source then
+        result
+
+    else
+        let
+            indexGenerator =
+                int 0 (List.length source - 1)
+
+            ( index, nextSeed ) =
+                step indexGenerator seed
+
+            valAtIndex =
+                getAt index source
+
+            sourceWithoutIndex =
+                removeAt index source
+        in
+        case valAtIndex of
+            Just val ->
+                shuffleListHelper nextSeed sourceWithoutIndex (val :: result)
+
+            Nothing ->
+                Debug.todo "generated an index outside list"
--- tests/MineSweeperTest.elm
+++ tests/MineSweeperTest.elm
@@ -1,8 +1,14 @@
-module MineSweeperTest exposing (testInitializationOfModel)
+module MineSweeperTest exposing (testDiscoverTile, testInitializationOfModel)
 
+import Array
 import Expect exposing (Expectation, FloatingPointTolerance(..))
 import Model.MineSweeper exposing (..)
+import Random exposing (Seed, int, step)
 import Test exposing (..)
+
+
+seed =
+    Random.initialSeed 42
 
 
 testInitializationOfModel : Test
@@ -21,7 +27,7 @@ testInitializationOfModel =
                         1
 
                     game =
-                        newGame x y mines
+                        newGame x y mines seed
                 in
                 Expect.equal (getXSize game) x
         , test "validate that y = 20" <|
@@ -37,7 +43,205 @@ testInitializationOfModel =
                         1
 
                     game =
-                        newGame x y mines
+                        newGame x y mines seed
                 in
                 Expect.equal (getYSize game) y
+        , test "test correct mine field size" <|
+            \_ ->
+                let
+                    x =
+                        10
+
+                    y =
+                        20
+
+                    mines =
+                        1
+
+                    game =
+                        newGame x y mines seed
+                in
+                Expect.equal (List.length (Array.toList (getTiles game))) (y * x)
+        , test "validate mines and field calculation" <|
+            \_ ->
+                let
+                    x =
+                        3
+
+                    y =
+                        5
+
+                    mines =
+                        5
+
+                    game =
+                        newGame x y mines seed
+
+                    expected =
+                        Array.fromList
+                            [ { bombsAround = 0, discovered = False, flagged = False, isBomb = True }
+                            , { bombsAround = 2, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 1, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 1, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 3, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 1, discovered = False, flagged = False, isBomb = True }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 3, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 2, discovered = False, flagged = False, isBomb = True }
+                            , { bombsAround = 1, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 3, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 2, discovered = False, flagged = False, isBomb = True }
+                            , { bombsAround = 1, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 1, discovered = False, flagged = False, isBomb = True }
+                            , { bombsAround = 2, discovered = False, flagged = False, isBomb = False }
+                            ]
+                in
+                Expect.equal (getTiles game) expected
+        ]
+
+
+testDiscoverTile : Test
+testDiscoverTile =
+    describe
+        "test discover tile 3x5"
+        [ test "discover (0,0)" <|
+            \_ ->
+                let
+                    x =
+                        3
+
+                    y =
+                        5
+
+                    mines =
+                        0
+
+                    game =
+                        discoverTile (newGame x y mines seed) 0 0
+
+                    expected =
+                        Array.fromList
+                            [ { bombsAround = 0, discovered = True, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            ]
+                in
+                Expect.equal (getTiles game) expected
+        , test "discover (1,1)" <|
+            \_ ->
+                let
+                    x =
+                        3
+
+                    y =
+                        5
+
+                    mines =
+                        0
+
+                    game =
+                        discoverTile (newGame x y mines seed) 1 1
+
+                    expected =
+                        Array.fromList
+                            [ { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = True, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            ]
+                in
+                Expect.equal (getTiles game) expected
+        , test "discover (2,2)" <|
+            \_ ->
+                let
+                    x =
+                        3
+
+                    y =
+                        5
+
+                    mines =
+                        0
+
+                    game =
+                        discoverTile (newGame x y mines seed) 2 2
+
+                    expected =
+                        Array.fromList
+                            [ { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = True, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            ]
+                in
+                Expect.equal (getTiles game) expected
+        , test "discover (1,4)" <|
+            \_ ->
+                let
+                    x =
+                        3
+
+                    y =
+                        5
+
+                    mines =
+                        0
+
+                    game =
+                        discoverTile (newGame x y mines seed) 1 4
+
+                    expected =
+                        Array.fromList
+                            [ { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = True, flagged = False, isBomb = False }
+                            , { bombsAround = 0, discovered = False, flagged = False, isBomb = False }
+                            ]
+                in
+                Expect.equal (getTiles game) expected
         ]
